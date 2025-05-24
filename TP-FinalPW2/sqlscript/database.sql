7d7812202b4dd1d1550464931c4d5158f2e0a5f4
YaninaKramar <yanu_kramar@yahoo.com>
2025-05-24 19:39:35 -0300

    base de datos

--- TP-FinalPW2/sqlscript/database.sql
+++ TP-FinalPW2/sqlscript/database.sql
@@ -1,33 +1,280 @@
-CREATE DATABASE labanda;
-USE labanda;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-05-2025 a las 03:50:30
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
-DROP TABLE IF EXISTS `canciones`;
-CREATE TABLE `canciones` (
-  `idCancion` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) DEFAULT NULL,
-  `duracion` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idCancion`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-INSERT INTO `canciones` VALUES (1,'cancion1',10),(2,'cancion2',12),(3,'cancion3',15);
 
-DROP TABLE IF EXISTS `presentaciones`;
-CREATE TABLE `presentaciones` (
-  `idPresentacion` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) DEFAULT NULL,
-  `fecha` datetime DEFAULT NULL,
-  `precio` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idPresentacion`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO `presentaciones` VALUES (1,'Presentacion 1','2020-06-02 22:02:14',10),(2,'Presentacion 2','2020-06-02 22:02:19',10),(3,'Presentacion 3','2020-06-02 22:02:21',10);
+--
+-- Base de datos: `preguntastico`
+--
+CREATE DATABASE IF NOT EXISTS `preguntastico` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `preguntastico`;
 
-create table integrantes
-(
-    nombre      text null,
-    instrumento text null,
-    id          int auto_increment
-        primary key
-);
+-- --------------------------------------------------------
 
-INSERT INTO integrantes(nombre, instrumento) VALUE ('facu', 'ukelele')
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+                             `id` int(11) NOT NULL,
+                             `nombre` varchar(30) NOT NULL,
+                             `color` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `partida`
+--
+
+CREATE TABLE `partida` (
+                           `id` int(11) NOT NULL,
+                           `fecha` date NOT NULL,
+                           `puntaje_final` int(11) NOT NULL,
+                           `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `partida_pregunta`
+--
+
+CREATE TABLE `partida_pregunta` (
+                                    `partida_id` int(11) NOT NULL,
+                                    `pregunta_id` int(11) NOT NULL,
+                                    `numero` int(11) NOT NULL,
+                                    `respondida_bien` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pregunta`
+--
+
+CREATE TABLE `pregunta` (
+                            `id` int(11) NOT NULL,
+                            `texto` text NOT NULL,
+                            `estado` varchar(30) NOT NULL,
+                            `categoria_id` int(11) NOT NULL,
+                            `creador_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reporte`
+--
+
+CREATE TABLE `reporte` (
+                           `id` int(11) NOT NULL,
+                           `estado` varchar(20) NOT NULL,
+                           `usuario_id` int(11) NOT NULL,
+                           `pregunta_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuesta`
+--
+
+CREATE TABLE `respuesta` (
+                             `pregunta_id` int(11) NOT NULL,
+                             `numero` int(11) NOT NULL,
+                             `texto` text NOT NULL,
+                             `es_correcta` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+                       `id` int(11) NOT NULL,
+                       `nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+                           `id` int(11) NOT NULL,
+                           `nombre_completo` varchar(255) NOT NULL,
+                           `fecha_nac` date NOT NULL,
+                           `sexo` varchar(100) NOT NULL,
+                           `pais` varchar(100) NOT NULL,
+                           `ciudad` varchar(100) NOT NULL,
+                           `email` varchar(100) NOT NULL,
+                           `contrasena` varchar(255) NOT NULL,
+                           `nombre_usuario` varchar(100) NOT NULL,
+                           `foto_perfil` text NOT NULL,
+                           `id_rol` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+    ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `partida`
+--
+ALTER TABLE `partida`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `partida_pregunta`
+--
+ALTER TABLE `partida_pregunta`
+    ADD PRIMARY KEY (`partida_id`,`pregunta_id`),
+  ADD KEY `pregunta_id` (`pregunta_id`);
+
+--
+-- Indices de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria_id` (`categoria_id`),
+  ADD KEY `creador_id` (`creador_id`);
+
+--
+-- Indices de la tabla `reporte`
+--
+ALTER TABLE `reporte`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `pregunta_id` (`pregunta_id`);
+
+--
+-- Indices de la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+    ADD UNIQUE KEY `pregunta_id` (`pregunta_id`,`numero`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+    ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_id_rol` (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `partida`
+--
+ALTER TABLE `partida`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reporte`
+--
+ALTER TABLE `reporte`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `partida`
+--
+ALTER TABLE `partida`
+    ADD CONSTRAINT `partida_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
+
+--
+-- Filtros para la tabla `partida_pregunta`
+--
+ALTER TABLE `partida_pregunta`
+    ADD CONSTRAINT `partida_pregunta_ibfk_1` FOREIGN KEY (`partida_id`) REFERENCES `partida` (`id`),
+  ADD CONSTRAINT `partida_pregunta_ibfk_2` FOREIGN KEY (`pregunta_id`) REFERENCES `pregunta` (`id`);
+
+--
+-- Filtros para la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+    ADD CONSTRAINT `pregunta_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`),
+  ADD CONSTRAINT `pregunta_ibfk_2` FOREIGN KEY (`creador_id`) REFERENCES `usuario` (`id`);
+
+--
+-- Filtros para la tabla `reporte`
+--
+ALTER TABLE `reporte`
+    ADD CONSTRAINT `reporte_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `reporte_ibfk_2` FOREIGN KEY (`pregunta_id`) REFERENCES `pregunta` (`id`);
+
+--
+-- Filtros para la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+    ADD CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`pregunta_id`) REFERENCES `pregunta` (`id`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+    ADD CONSTRAINT `fk_id_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
